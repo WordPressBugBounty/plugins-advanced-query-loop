8dea4a1c710bd540e18bce6edb9c4dbe45c43cf5
Sean Murphy <sean@iamseanmurphy.com>
2024-10-02 13:28:45 +0000

    3.1.1

---
+++ advanced-query-loop/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ryanwelcher/advanced-query-loop',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => '54b654d116f8e8763114b0421eb38c808043000b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ryanwelcher/advanced-query-loop' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => '54b654d116f8e8763114b0421eb38c808043000b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
