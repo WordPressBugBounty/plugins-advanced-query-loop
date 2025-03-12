72c7cd0d35ac61942f9d0304ca192de5d6e9f0f8
Sean Murphy <sean@iamseanmurphy.com>
2025-03-12 19:17:50 +0000

    4.1.0

--- advanced-query-loop/build/variations.asset.php
+++ advanced-query-loop/build/variations.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '22c59bd74403b6cbc3a5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '809cfa0a1939e92348e0');
--- advanced-query-loop/vendor/composer/installed.php
+++ advanced-query-loop/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ryanwelcher/advanced-query-loop',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '2ce77d410e60194bf852084ee9fb381741a3e170',
+        'reference' => '2a7cc1d62a8dbc36c6899e407a2cb60a9c8f5043',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'ryanwelcher/advanced-query-loop' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '2ce77d410e60194bf852084ee9fb381741a3e170',
+            'reference' => '2a7cc1d62a8dbc36c6899e407a2cb60a9c8f5043',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
